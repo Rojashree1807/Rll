f936d47eea4d9203f1a56c5700b6f7c3587a07a7
Rojashree1807 <rojarojashree1807@gmail.com>
2024-03-01 12:52:33 +0530

    Initial commit

---
+++ ClinicApi/Controllers/DoctorController.cs
@@ -0,0 +1,87 @@
+﻿using ClinicApi.Models;
+using DALLibrary.Domain_Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace ClinicApi.Controllers
+{
+    public class DoctorController : ApiController
+    {
+        private readonly Service service;//object define
+        public DoctorController()
+        {
+            service = new Service();//object create
+        }
+        // GET: api/DoctorsApi
+        public List<Doctor> GetDoctors()
+        {
+
+            return service.GetAllDoctors();
+        }
+
+        // GET: api/DoctorsApi/5
+        [ResponseType(typeof(Doctor))]
+        public IHttpActionResult GetDoctor(int id)
+        {
+            Doctor doctor = service.GetDoctorById(id);
+            if (doctor == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(doctor);
+        }
+
+        // PUT: api/DoctorsApi/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutDoctor(int id, Doctor doctor)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != doctor.DoctorId)
+            {
+                return BadRequest();
+            }
+
+            service.UpdateDoctor(doctor);
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/DoctorsApi
+        [ResponseType(typeof(Doctor))]
+        public IHttpActionResult PostDoctor(Doctor doctor)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            service.AddDoctor(doctor);
+
+            return CreatedAtRoute("DefaultApi", new { id = doctor.DoctorId }, doctor);
+        }
+
+        // DELETE: api/DoctorsApi/5
+        [ResponseType(typeof(Doctor))]
+        public IHttpActionResult DeleteDoctor(int id)
+        {
+            Doctor doctor = service.GetDoctorById(id);//find doctor ,if found it removes ,otherwise notfound
+            if (doctor == null)
+            {
+                return NotFound();
+            }
+
+            service.DeleteDoctor(id);
+            return Ok(doctor);
+        }
+
+    }
+}
---
+++ DALLibrary/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace DALLibrary.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<DALLibrary.Data.ClinicDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(DALLibrary.Data.ClinicDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
